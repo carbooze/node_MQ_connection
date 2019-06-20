5c3815d575803c88bd8cc1ec5f69706aa85f4730
AF74391 <basanth.kumar@anthem.com>
2019-06-20 13:09:23 -0400

    update

---
+++ connect.js
@@ -0,0 +1,103 @@
+
+'use strict';
+/*
+  Copyright (c) IBM Corporation 2017, 2018
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+   Contributors:
+     Mark Taylor - Initial Contribution
+*/
+
+/*
+ * This is an example of a Node.js program to connect as a client to an
+ * IBM MQ queue manager.
+ *
+ * The queue manager name and channel attributes such as CONNAME are hardcoded
+ * to demonstrate how to use them.
+ *
+ * This program also demonstrates how authentication can be achieved with
+ * a userid/password option
+ */
+
+// Import the MQ package
+var mq = require('ibmmq');
+var MQC = mq.MQC; // Want to refer to this export directly for simplicity
+
+// The queue manager to be used.
+var qMgr = "DWEAA00";
+var hConn;
+
+function formatErr(err) {
+  return  "MQ call failed in " + err.message;
+}
+
+// When we're done, close queues and connections
+function cleanup(hConn) {
+}
+
+function sleep(ms) {
+  return new Promise(resolve=>{
+    setTimeout(resolve,ms);
+  });
+}
+
+
+// The program starts here.
+// Connect to the queue manager.
+console.log("Sample AMQSCONN.JS start");
+
+// Create default MQCNO structure
+var cno = new mq.MQCNO();
+
+// Add authentication via the MQCSP structure
+var csp = new mq.MQCSP();
+csp.UserId = "weausr1";
+csp.Password = "";
+// Make the MQCNO refer to the MQCSP
+// This line allows use of the userid/password
+cno.SecurityParms = csp;
+
+// And use the MQCD to programatically connect as a client
+// First force the client mode
+cno.Options |= MQC.ClientConn;
+// And then fill in relevant fields for the MQCD
+var cd = new mq.MQCD();
+cd.ConnectionName = "va10dlvihs308.wellpoint.com(57084)";
+cd.ChannelName = "TO.DWEAA00.A";
+// Make the MQCNO refer to the MQCD
+cno.ClientConn = cd;
+
+// MQ V9.1.2 allows setting of the application name explicitly
+if (MQC.MQCNO_CURRENT_VERSION >= 7) {
+  cno.ApplName = "Node.js 9.1.2 ApplName";
+}
+
+// Now we can try to connect
+mq.Connx(qMgr, cno, function(err,conn) {
+    console.log("test");
+
+  if (err) {
+    console.log("test_1");
+    console.log(formatErr(err));
+  } else {
+    console.log("MQCONN to %s successful ", qMgr);
+    // Sleep for a few seconds - bad in a real program but good for this one
+    sleep(3 *1000).then(() => {
+      mq.Disc(conn, function(err) {
+        if (err) {
+
+          console.log(formatErr(err));
+        } else {
+          console.log("MQDISC successful");
+        }
+      });
+    });
+  }
+});
---
+++ connect2.js
@@ -0,0 +1,72 @@
+    var mq = require('ibmmq');
+    var MQC = mq.MQC; // Want to refer to this export directly for simplicity
+  
+    // The queue manager and queue to be used. These can be overridden on command line.
+    var qMgr = "DWEAA00";
+    var qName = "CMGWY.AIM.REPLY.DEV";
+    var mqmd = new mq.MQMD(); // Defaults are fine.
+    var pmo = new mq.MQPMO();
+    var cd = new mq.MQCD();
+    var cno = new mq.MQCNO();
+  
+    cd.ConnectionName = "va10dlvihs308.wellpoint.com(57084)";
+    cd.ChannelName = "TO.DWEAA00.A";
+    var csp = new mq.MQCSP();
+  
+    cno.ClientConn = cd;  
+    cno.Options = MQC.MQCNO_CLIENT_BINDING; // use MQCNO_CLIENT_BINDING to connect as client
+  
+    function putMessage(hObj) {
+    //  var msg = Buffer.from(JSON.stringify(coff));
+    var msg = "testsdgfhsdgf"
+    // Describe how the Put should behave
+
+      pmo.Options = MQC.MQPMO_NO_SYNCPOINT |
+                    MQC.MQPMO_NEW_MSG_ID |
+                    MQC.MQPMO_NEW_CORREL_ID;
+  
+      mq.Put(hObj,mqmd,pmo,msg,function(err) {
+        if (err) {
+            console.log("test");
+          console.log(formatErr(err));
+        } else {
+            console.log("test");
+          console.log("MQPUT successful");
+        }
+      });
+    }
+    mq.Connx(qMgr, cno, function (err, hConn) {
+      if (err) {
+        console.log((err));
+      } else {
+        console.log("MQCONN to %s successful ", qMgr);
+  
+        // Define what we want to open, and how we want to open it.
+        var od = new mq.MQOD();
+        od.ObjectName = qName;
+        od.ObjectType = MQC.MQOT_Q;
+        var openOptions = MQC.MQOO_OUTPUT;
+        mq.Stat(hConn, od, function (err, hObj1) {
+            console.log("MQOPEN of %s successful", qName);
+            if (err) {
+              console.log(formatErr(err));
+            } else {
+                console.log(hObj1.Stat);
+              console.log("MQOPEN of %s successful", qName);
+
+              //putMessage(hObj);
+            }
+           // cleanup(hConn, hObj);
+          });
+        
+        mq.Open(hConn, od, openOptions, function (err, hObj) {
+          if (err) {
+            console.log(formatErr(err));
+          } else {
+            console.log("MQOPEN of %s successful", qName);
+            putMessage(hObj);
+          }
+         // cleanup(hConn, hObj);
+        });
+      }
+    });
